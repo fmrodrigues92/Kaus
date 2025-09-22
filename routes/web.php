db6d400f4069287c5a2904b323c85a9885917801
Fernando Menezes <fmrodrigues92@yahoo.com>
2025-09-22 10:30:59 -0300

    melhorando estrutura de agrupamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::group([
+    'as' => '',
+    'prefix' => '',
+    'middleware' => ['web', 'verified']
+], function () {
+    
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
@@ -20,6 +25,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('meals', [MealController::class, 'index'])->name('meals.index');
     Route::post('meals', [MealController::class, 'store'])->name('meals.store');
 });
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
